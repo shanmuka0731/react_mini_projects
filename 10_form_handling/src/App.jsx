7bef208b21e117dc6700da823b786909c251ca24
Shanmuka Priya <shanmukapriyasalapu@gmail.com>
2026-05-25 20:39:17 +0530

    form_handling

---
+++ 10_form_handling/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+  
+  const onSubmitHandler=(elem)=>{
+    elem.preventDefault() // used to prevent the default behaviour of the form which is to refresh the page
+    console.log("HI FRIENDS")
+
+  }
+
+
+  return(
+    <div >
+      <form className="hello" onSubmit={
+      (elem)=>{
+      onSubmitHandler(elem)
+    }
+      }>
+      <input onChange={(elem)=>{
+        console.log(elem.target.value)
+      }} className="hello2" type="text" placeholder="Enter the Input"/>
+      <button>Submit</button>
+      </form>
+    </div>
+    )
+}
+
+export default App
