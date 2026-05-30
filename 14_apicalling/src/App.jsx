bad802dd0ed7f629211330fdb0981a61b019db47
Shanmuka Priya <shanmukapriyasalapu@gmail.com>
2026-05-30 23:02:41 +0530

    understanding api calling

--- 14_apicalling/src/App.jsx
+++ 14_apicalling/src/App.jsx
@@ -1,4 +1,4 @@
- import React from 'react'
+ import React, { useState } from 'react'
  import axios from 'axios'
  const App = () => {
   
@@ -29,8 +29,6 @@
       
 
   }
-
-
    return (
      <div>
       <button onClick={getdata}>
