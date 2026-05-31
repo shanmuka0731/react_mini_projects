e77c92c37f997b658f5c5649481b4de27bf74fd4
Shanmuka Priya <shanmukapriyasalapu@gmail.com>
2026-05-31 16:17:16 +0530

    useEffect Explanation

---
+++ 15_useEffect/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+const App = () => {
+   
+  const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(100)
+  
+  useEffect(() => {
+    console.log("Hi Guys UseEffect is working");
+    
+  }, [num])
+  
+  return (
+    <div>
+      <h1>num is {num}</h1>
+      <h2>num2 is {num2}</h2>
+      <button onMouseEnter={()=>{setNum(num+1)}} onMouseLeave={()=>{setNum2(num2+1)}}>Click</button>
+    </div>
+  )
+}
+
+export default App
