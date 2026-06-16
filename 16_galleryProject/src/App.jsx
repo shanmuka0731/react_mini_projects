abaeee5a77eb6b3844a211742b6741a524193f97
Shanmuka Priya <shanmukapriyasalapu@gmail.com>
2026-06-16 16:42:23 +0530

    Gallery Project Updated

--- 16_galleryProject/src/App.jsx
+++ 16_galleryProject/src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const [userdata, setUserdata] = useState([])
+  const [index,setIndex] = useState(1)
+
+  const getdata=async()=>{
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+    setUserdata(response.data)
+  }
+
+  useEffect(()=>{getdata()},[index])
+  
+  let printdata=<h3 className = " text-2xl text-amber-500 absolute top-1/2 left-1/2 -translate-x-1.5 -translate-y-1.5 ">Loading...</h3>
+  if(userdata.length>0){
+    printdata= userdata.map((elem,idx)=>{
+      return(
+        <div key={idx}>
+          <a href={elem.url} target="_blank">
+            <div className="h-50 w-64 m-4 bg-white rounded-xl overflow-hidden">
+            <img className="h-full w-full object-cover"src={elem.download_url} alt={elem.id} />
+            </div>
+            <h3 className="text-white ml-6">{elem.author}</h3>
+          </a>
+        </div>
+        
+      )
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen overflow-auto  bg-black text-white p-7'> 
+        <div className="flex flex-wrap h-[82%]">{printdata}</div>
+        
+        <div className="flex justify-center items-center gap-6 p-4">
+          <button
+          onClick={()=>{
+            if(index>1){
+            setIndex(index-1)
+            setUserdata([])
+            }             
+
+          }}
+          className="bg-amber-400 text-black text-sm px-4 py-2 font-semibold" >Prev</button>
+          <h4>Page {index}</h4>
+          <button 
+          onClick={()=>{
+            setUserdata([])
+            setIndex(index+1)
+          }}
+          className="bg-amber-400 text-black text-sm px-4 py-2 font-semibold">Next</button>
+        </div>
+    </div>
   )
 }
 
